8c74b30b9da354d1df0fa96844b74d65eef4dcb6
SunnyPanda <kashtanka.nya@gmail.com>
2022-12-25 00:36:45 +0200

    Stage 3/7: Count them all

--- Smart Calculator/task/src/calculator/Main.scala
+++ Smart Calculator/task/src/calculator/Main.scala
@@ -5,7 +5,11 @@ import scala.io.StdIn.readLine
 object Main extends App {
     var input = readLine()
     while (input != "/exit") {
-        if (input.nonEmpty) println(input.split(" ").map(_.toInt).sum)
+        input match {
+            case "/help" => println("The program calculates the sum of numbers")
+            case "" => ()
+            case _ => println(input.split(" ").map(_.toInt).sum)
+        }
         input = readLine()
     }
     println("Bye!")
